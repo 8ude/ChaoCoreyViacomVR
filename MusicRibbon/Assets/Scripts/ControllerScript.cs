f9401a9d2f8afb73a8f91802e44fc15c8c1f3642
Chaojin <jinchaoonly@gmail.com>
2017-06-10 16:54:21 -0400

    Rename script

--- MusicRibbon/Assets/Scripts/ControllerScript.cs
+++ MusicRibbon/Assets/Scripts/ControllerScript.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drawmusic : MonoBehaviour {
+public class ControllerScript : MonoBehaviour {
 
 	public bool triggerButtonDown = false;
 	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 	private SteamVR_Controller.Device controller{
-		get{ return SteamVR_Controller.Input ((int)SteamVR_TrackedObject.index); }
+		get{return SteamVR_Controller.Input ((int)TrackedObj.index);
+		}
 	}
 
 	private SteamVR_TrackedObject TrackedObj;
 
 	// Use this for initialization
 	void Start () {
-		TrackedObj = GetComponent ();
+		//TrackedObj = GetComponent();
 	}
 	
 	// Update is called once per frame
