2b69f53fe7b560b3f364a67124d7ee0768170e9b
Chaojin <jinchaoonly@gmail.com>
2017-06-10 14:50:19 -0400

    add trigger compotent

---
+++ MusicRibbon/Assets/Drawmusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawmusic : MonoBehaviour {
+
+	public bool triggerButtonDown = false;
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	private SteamVR_Controller.Device controller{
+		get{ return SteamVR_Controller.Input ((int)SteamVR_TrackedObject.index); }
+	}
+
+	private SteamVR_TrackedObject TrackedObj;
+
+	// Use this for initialization
+	void Start () {
+		TrackedObj = GetComponent ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (triggerButtonDown) {
+			
+			this.gameObject.GetComponent<TrailRenderer> ().enabled = true;
+		}
+		
+	}
+}
