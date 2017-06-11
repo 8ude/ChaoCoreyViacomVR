ed1871252fedc7ccb4d917f913e11d113838c6cb
8ude <crbertelsen@gmail.com>
2017-06-11 12:56:22 -0400

    Added Tweening Library

--- MusicRibbon/Assets/Scripts/RibbonMotion.cs
+++ MusicRibbon/Assets/Scripts/RibbonMotion.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class RibbonMotion : MonoBehaviour {
 
 	SpectrumAnalysis analyzer;
+	float prevAnalyzerValue, prevYPos;
+	public float dampening = 0.1f;
+	public float smoothingValue = 0.1f;
 
 
 	// Use this for initialization
 	void Start () {
+		prevAnalyzerValue = 0f;
+		prevYPos = transform.position.y;
 		analyzer = GetComponentInParent<SpectrumAnalysis> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		transform.position = new Vector3 (transform.position.x, 1f+(analyzer.GetWholeEnergy () * 0.1f), transform.position.z);
-		
+		float nextYPos = (analyzer.GetWholeEnergy () * dampening) - prevAnalyzerValue;
+
+		transform.position = new Vector3 (transform.position.x, 1f+(nextYPos * smoothingValue), transform.position.z);
+
+		prevAnalyzerValue = analyzer.GetWholeEnergy () * dampening;
+
 	}
 }
