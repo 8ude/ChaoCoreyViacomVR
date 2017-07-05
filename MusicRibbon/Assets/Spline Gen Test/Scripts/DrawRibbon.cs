908364d56918b2bdcd1559dff7ccd07c0f2e90a0
8ude <crbertelsen@gmail.com>
2017-07-05 18:13:08 -0400

    draw mesh with audio

--- MusicRibbon/Assets/Spline Gen Test/Scripts/DrawRibbon.cs
+++ MusicRibbon/Assets/Spline Gen Test/Scripts/DrawRibbon.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine;
-using Curvy;
+using FluffyUnderware.Curvy;
+using FluffyUnderware.Curvy.Generator;
+using FluffyUnderware.Curvy.Generator.Modules;
 
 public class DrawRibbon: MonoBehaviour {
 
 	private SteamVR_TrackedController device;
 	public float timeInterval = 0f;
 	public float pointGenTime = 0.1f;
+	public int numRibbons = 0;
+
+
 
 	public GameObject markerPrefab;
 	List<GameObject> markerChain;
 	public GameObject markerParentPrefab;
+	public GameObject curvySplinePrefab;
+	public GameObject ribbonSoundPrefab;
     
+	GameObject currentRibbonSound;
+
+	//public CurvySpline ribbonSpline;
 
 
 	// Use this for initialization
@@ -31,13 +41,37 @@ public class DrawRibbon: MonoBehaviour {
 	{
 		Debug.Log ("trigger pressed");	
 
+		if (currentRibbonSound) {
+			currentRibbonSound = null;
+		}
 
+		currentRibbonSound = Instantiate (ribbonSoundPrefab, transform.position, Quaternion.identity);
+		currentRibbonSound.GetComponent<DrawRibbonSound> ().clipIndex = numRibbons % currentRibbonSound.GetComponent<DrawRibbonSound> ().origClips.Length;
+		currentRibbonSound.GetComponent<DrawRibbonSound> ().StartDrawingRibbon ();
+
+
+		numRibbons++;
 
 	}
 
 	void TriggerReleased(object sender, ClickedEventArgs e) {
 
-		GameObject parentObject = Instantiate(markerParentPrefab, transform.position, Quaternion.identity);
+		currentRibbonSound.GetComponent<DrawRibbonSound> ().StopDrawingRibbon ();
+
+		GameObject parentObject = Instantiate(markerParentPrefab, Vector3.zero, Quaternion.identity);
+		CurvyGenerator generator = parentObject.GetComponent<CurvyGenerator> ();
+
+		GameObject splineObject = Instantiate (curvySplinePrefab, transform.position, Quaternion.identity);
+		CurvySpline spline = splineObject.GetComponent<CurvySpline> ();
+
+		var isp = generator.GetComponentInChildren<InputSplinePath> ();
+
+		
+
+		
+
+		//CurvySpline ribbonSpline = parentObject.GetComponent<CurvySpline> ();
+
         
         Vector3[] newPoints = new Vector3[markerChain.Count];
         int i = 0;
@@ -45,10 +79,19 @@ public class DrawRibbon: MonoBehaviour {
 
             newPoints[i] = go.transform.position;
             i++;
-            go.transform.SetParent (parentObject.transform);
+			go.transform.SetParent (spline.transform);
             
             
 		}
+
+		spline.Add (newPoints);
+
+		isp.Spline = spline;
+			
+		//ribbonSpline.Add (newPoints);
+
+		//InputSplinePath inputSpline = parentObject.GetComponentInChildren<InputSplinePath> ();
+		//inputSpline.Spline = ribbonSpline;
        
 		markerChain.Clear ();
 		Debug.Log ("trigger released?");
@@ -66,6 +109,12 @@ public class DrawRibbon: MonoBehaviour {
 			markerChain.Add (newMarker);
 			timeInterval = 0f;
 		}
+
+	}
+
+	void RibbonSoundCreator() {
+
+
 
 	}
 
---
+++ MusicRibbon/Assets/Spline Gen Test/Scripts/DrawRibbonSound.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawRibbonSound : MonoBehaviour {
+
+	AudioSource mySource;
+	public AudioClip[] origClips;
+
+	public int clipIndex = 0;
+
+	float startTime, stopTime;
+	float timeDifference;
+
+	int sampleOffset = 0;
+
+	//float[] audioData;
+	float[] origAudioData;
+
+	void Start() {
+		
+
+
+	}
+
+	public void StartDrawingRibbon() {
+
+		mySource = GetComponent<AudioSource> ();
+		mySource.clip = origClips[clipIndex];
+		if (clipIndex == 2) {
+			mySource.time = 30f;
+		} else if (clipIndex == 3) {
+			mySource.time = 60f;
+		}
+		mySource.Play ();
+
+
+		origAudioData = new float[mySource.clip.samples*mySource.clip.channels];
+		if (clipIndex == 2) {
+			//offset for drum loop
+			sampleOffset = Mathf.RoundToInt (30 * origClips [clipIndex].frequency * origClips [clipIndex].channels);
+
+		} else if (clipIndex == 3) {
+			sampleOffset = Mathf.RoundToInt (60 * origClips [clipIndex].frequency * origClips [clipIndex].channels);
+		}
+		mySource.clip.GetData (origAudioData, sampleOffset);
+
+		startTime = Time.time;
+
+	}
+
+	public void StopDrawingRibbon() {
+		stopTime = Time.time;
+
+		float newClipLength = stopTime - startTime;
+		int newClipSamples = Mathf.RoundToInt (newClipLength * origClips[clipIndex].frequency);
+
+		//Debug.Log ("Clip Samples:" + newClipSamples);
+
+		float[] audioData = new float[newClipSamples*mySource.clip.channels];
+
+		AudioClip newClip = AudioClip.Create ("RibbonClip", newClipSamples, origClips[clipIndex].channels, origClips[clipIndex].frequency, false);
+
+
+
+		for (int i = 0; i < (newClipSamples*mySource.clip.channels); i++) {
+
+			audioData [i] = origAudioData [i];
+
+
+
+		}
+
+		//Debug.Log ("10000th sample:" + audioData[10000]);
+
+		newClip.SetData (audioData, 0);
+
+
+
+		mySource.clip = newClip;
+
+		mySource.Play ();
+
+	}
+
+
+}
