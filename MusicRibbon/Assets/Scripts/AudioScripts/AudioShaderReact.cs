12d48c414a13a04fd66543aa21a02024574c9a0b
David Frisch <dmf463@nyu.edu>
2017-07-08 14:36:43 -0400

    Audio reactive shaders that don't work

---
+++ MusicRibbon/Assets/Scripts/AudioScripts/AudioShaderReact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioShaderReact : MonoBehaviour {
+
+    SpectrumAnalysis analyzer;
+
+    float normalizedEnergy = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+        analyzer = GetComponent<SpectrumAnalysis>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        normalizedEnergy = Mathf.Clamp01(analyzer.GetWholeEnergy());
+        //Debug.Log(normalizedEnergy);
+
+        Shader.SetGlobalFloat("_AudioInput", normalizedEnergy * 5f);
+        Debug.Log(Shader.GetGlobalFloat("_AudioInput"));
+
+	}
+}
