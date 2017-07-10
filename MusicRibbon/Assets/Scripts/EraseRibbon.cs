595c91b38c8c5f8523a27aafb197a83f57e7b741
Chaojin <jinchaoonly@gmail.com>
2017-07-09 20:46:04 -0400

    switch between rubber and wand

--- MusicRibbon/Assets/Scripts/EraseRibbon.cs
+++ MusicRibbon/Assets/Scripts/EraseRibbon.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class EraseRibbon : MonoBehaviour {
 
 	public bool isErasing;
+	public GameObject LeftController;
+	public GameObject RightController;
+	public GameObject LeftWand;
+	public GameObject LeftRubber;
+	public GameObject RightWand;
+	public GameObject RightRubber;
 
 	// Use this for initialization
 	void Start () {
 
 		isErasing = false;
+
+		LeftController = GameObject.Find ("LeftController");
+		RightController = GameObject.Find ("RightController");
+
+		LeftWand = LeftController.transform.Find ("Wand").gameObject;
+		LeftRubber = LeftController.transform.Find ("Capsule").gameObject;
+
+		RightWand = RightController.transform.Find ("Wand").gameObject;
+		RightRubber = RightController.transform.Find ("Capsule").gameObject;
 		
 	}
 	
@@ -20,11 +35,38 @@ public class EraseRibbon : MonoBehaviour {
 
 	public void EnableErasing(){
 		isErasing = true;
+
 	}
 
 	public void DisableErasing(){
 		isErasing = false;
 	}
+
+	public void EnableLeftRubber(){
+		LeftWand.SetActive(false);
+		LeftRubber.SetActive(true);
+	}
+
+	public void DisableLeftRubber(){
+		LeftWand.SetActive(true);
+		LeftRubber.SetActive(false);
+		
+	}
+
+	public void EnableRightRubber(){
+		RightWand.SetActive (false);
+		RightRubber.SetActive(true);
+	
+	}
+
+	public void DiableRightRubber(){
+		RightWand.SetActive (true);
+		RightRubber.SetActive(false);
+	
+	}
+
+
+
 
 	void OnTriggerEnter(Collider other){
 
