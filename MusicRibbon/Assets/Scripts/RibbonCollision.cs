3a96d482eec17d58c9a53a508eb4152e2085ae6c
8ude <crbertelsen@gmail.com>
2017-09-10 19:18:13 -0400

    Trying new Melody interaction
    just audio for now

--- MusicRibbon/Assets/Scripts/RibbonCollision.cs
+++ MusicRibbon/Assets/Scripts/RibbonCollision.cs
@@ -10,35 +10,37 @@ using UnityEngine.Audio;
 public class RibbonCollision : MonoBehaviour {
 
     public GameObject particlePrefab;
+    public AudioSource mySource;
+    AudioClip melodyClip;
     
     //the distance along the wand where the particles will spawn
-	public float yOffset;
+    public float yOffset;
 
     // use DrawRibbon.cs to tell if we are erasing or not
-	DrawRibbon drawRibbonScript;
+    DrawRibbon drawRibbonScript;
 
     bool playingMicroSample = false;
 
     //using this to make sure we cycle through the collision audio clips
     int clipIndex;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-		drawRibbonScript = transform.root.GetComponentInChildren<DrawRibbon> ();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        drawRibbonScript = transform.root.GetComponentInChildren<DrawRibbon> ();
+        melodyClip = null;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
 
     void OnTriggerEnter(Collider other) {
 
         //Debug.Log("collision occured");
 
-		if (other.transform.parent != null && !drawRibbonScript.eraseRibbon.isErasing) {
+        if (other.transform.parent != null && !drawRibbonScript.eraseRibbon.isErasing) {
             if (other.transform.parent.parent.gameObject.tag == "MarkerParent") {
 
                 //Debug.Log("wand-ribbon collision occured");
@@ -50,23 +52,22 @@ public class RibbonCollision : MonoBehaviour {
 
                 //if we hit the melody stem, we want a different kind of interaction
 
-                if(collidedStemType != RibbonGenerator.musicStem.Melody) {
+                if (collidedStemType != RibbonGenerator.musicStem.Melody) {
 
                     GameObject newParticles = Instantiate(particlePrefab, transform.position + (transform.up * yOffset), Quaternion.identity);
                     AudioSource aSource = newParticles.GetComponent<AudioSource>();
-					switch (collidedStemType)
-					{
-						case RibbonGenerator.musicStem.Bass:
-							aSource.clip = RibbonGameManager.instance.bassCollisionClips[clipIndex % RibbonGameManager.instance.bassCollisionClips.Length];
-							break;
-						case RibbonGenerator.musicStem.Drum:
-							aSource.clip = RibbonGameManager.instance.drumCollisionClips[clipIndex % RibbonGameManager.instance.drumCollisionClips.Length];
-							break;
-						case RibbonGenerator.musicStem.Harmony:
-							aSource.clip = RibbonGameManager.instance.harmonyCollisionClips[clipIndex % RibbonGameManager.instance.harmonyCollisionClips.Length];
-							break;
+                    switch (collidedStemType) {
+                        case RibbonGenerator.musicStem.Bass:
+                            aSource.clip = RibbonGameManager.instance.bassCollisionClips[clipIndex % RibbonGameManager.instance.bassCollisionClips.Length];
+                            break;
+                        case RibbonGenerator.musicStem.Drum:
+                            aSource.clip = RibbonGameManager.instance.drumCollisionClips[clipIndex % RibbonGameManager.instance.drumCollisionClips.Length];
+                            break;
+                        case RibbonGenerator.musicStem.Harmony:
+                            aSource.clip = RibbonGameManager.instance.harmonyCollisionClips[clipIndex % RibbonGameManager.instance.harmonyCollisionClips.Length];
+                            break;
 
-					}
+                    }
                     aSource.Play();
                     ParticleSystem ps = newParticles.GetComponent<ParticleSystem>();
                     var main = ps.main;
@@ -76,12 +77,78 @@ public class RibbonCollision : MonoBehaviour {
 
                     Destroy(newParticles, 2.0f);
                     clipIndex++;
-                } else {
-                    //melody ribbon - touching ribbon will create microsampled fragment
                 }
-
+                else {
+                    //melody ribbon - touching ribbon will create microsampled fragment
+                    playingMicroSample = true;
+                    MarkerObjectBehavior[] markers = other.GetComponentsInChildren<MarkerObjectBehavior>();
+                    //find closest marker
+                    float[] markerDistances = new float[markers.Length];
+                    int closestMarkerIndex = 0;
+                    float closestMarkerDistance = 3f;
+                    for (int i = 0; i < markers.Length; i++) {
+                        markerDistances[i] = Vector3.Distance(markers[i].gameObject.transform.position, transform.position);
+                        if (markerDistances[i] < closestMarkerDistance) {
+                            closestMarkerIndex = i;
+                            closestMarkerDistance = markerDistances[i];
+                        }
+                    }
+                    melodyClip = other.transform.parent.GetComponentInChildren<DrawRibbonSound>().mySource.clip;
+                    mySource.clip = MicroClipMaker.MakeMicroClip(melodyClip, markers.Length, closestMarkerIndex, Mathf.Clamp(1f / closestMarkerDistance, 0.2f, 1f));
+                    mySource.Play();
+                }
             }
         }
+    }
+    void OnTriggerStay(Collider other) {
 
+
+        if (other.transform.parent != null && !drawRibbonScript.eraseRibbon.isErasing) {
+            if (other.transform.parent.parent.gameObject.tag == "MarkerParent") {
+
+            
+
+                //set the audio clip in accordance with the collision audio in the game manager
+
+                RibbonGenerator collidedGenerator = other.transform.root.GetComponent<RibbonGenerator>();
+                RibbonGenerator.musicStem collidedStemType = collidedGenerator.myStem;
+
+                //if we hit the melody stem, we want a different kind of interaction
+
+
+                if (collidedStemType == RibbonGenerator.musicStem.Melody && !mySource.isPlaying) {
+                    //melody ribbon - touching ribbon will create microsampled fragment
+                    playingMicroSample = true;
+                    MarkerObjectBehavior[] markers = other.GetComponentsInChildren<MarkerObjectBehavior>();
+                    //find closest marker
+                    float[] markerDistances = new float[markers.Length];
+                    int closestMarkerIndex = 0;
+                    float closestMarkerDistance = 3f;
+                    for (int i = 0; i < markers.Length; i++) {
+                        markerDistances[i] = Vector3.Distance(markers[i].gameObject.transform.position, transform.position);
+                        if (markerDistances[i] < closestMarkerDistance) {
+                            closestMarkerIndex = i;
+                            closestMarkerDistance = markerDistances[i];
+                        }
+                    }
+                    melodyClip = other.transform.parent.GetComponentInChildren<DrawRibbonSound>().mySource.clip;
+                    mySource.clip = MicroClipMaker.MakeMicroClip(melodyClip, markers.Length, closestMarkerIndex, Mathf.Clamp(1f / closestMarkerDistance, 0.2f, 1f));
+                    mySource.Play();
+                }
+            }
+        }
+        
+    }
+    void OnTriggerExit(Collider other) {
+        if (other.transform.parent != null && !drawRibbonScript.eraseRibbon.isErasing) {
+            if (other.transform.parent.parent.gameObject.tag == "MarkerParent") {
+
+                melodyClip = null;
+                mySource.Stop();
+                mySource.clip = null;
+                
+            }
+        }
+        
     }
 }
