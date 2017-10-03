29d4898e9c7b954acd64a622819ce01c9d04e8a2
Chaojin <jinchaoonly@gmail.com>
2017-10-02 20:45:21 -0400

    I hope now the marker object with have the same color with the ribbons.

--- MusicRibbon/Assets/Scripts/Ribbon/MarkerObjectBehavior.cs
+++ MusicRibbon/Assets/Scripts/Ribbon/MarkerObjectBehavior.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MarkerObjectBehavior : MonoBehaviour {
+
+	public new List<Color> OriginalColors;
+	public new List<Color> MountainColors;
+	public new List<Color> DesertColors;
+	public new List<Color> SnowColors;
+	public new List<Color> SeaColors;
+
+	public Color drumColor;
+	public Color bassColor;
+	public Color melodyColor;
+	public Color harmonyColor;
+
 
 	public Material DrumRibbonMaterial;
 	public Material BassRibbonMaterial;
@@ -16,6 +29,53 @@ public class MarkerObjectBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		if(SceneManager.GetActiveScene().name == "Echo"){
+
+		drumColor = OriginalColors[0];
+		bassColor = OriginalColors[1];
+		melodyColor = OriginalColors[2];
+		harmonyColor = OriginalColors[3];
+
+		}
+
+		if(SceneManager.GetActiveScene().name == "Mountain"){
+
+			drumColor = MountainColors[0];
+			bassColor = MountainColors[1];
+			melodyColor = MountainColors[2];
+			harmonyColor = MountainColors[3];
+
+		}
+
+		if(SceneManager.GetActiveScene().name == "Desert"){
+
+			drumColor = DesertColors[0];
+			bassColor = DesertColors[1];
+			melodyColor = DesertColors[2];
+			harmonyColor = DesertColors[3];
+
+		}
+
+		if(SceneManager.GetActiveScene().name == "Snow"){
+
+			drumColor = SnowColors[0];
+			bassColor = SnowColors[1];
+			melodyColor = SnowColors[2];
+			harmonyColor = SnowColors[3];
+
+		}
+
+		if(SceneManager.GetActiveScene().name == "Sea"){
+
+			drumColor = SeaColors[0];
+			bassColor = SeaColors[1];
+			melodyColor = SeaColors[2];
+			harmonyColor = SeaColors[3];
+
+		}
+
+
 		
 	}
 
@@ -27,6 +87,7 @@ public class MarkerObjectBehavior : MonoBehaviour {
 	public void DrumMarkerObject(){
 		
 		this.gameObject.GetComponent<MeshRenderer> ().material = DrumRibbonMaterial;
+		this.gameObject.GetComponent<MeshRenderer> ().material.color = drumColor;
 		this.gameObject.GetComponent<MeshFilter> ().mesh = DrumMesh.gameObject.GetComponent<MeshFilter> ().sharedMesh;
 
 
@@ -35,6 +96,7 @@ public class MarkerObjectBehavior : MonoBehaviour {
 	public void BassMarkerObject(){
 		
 		this.gameObject.GetComponent<MeshRenderer> ().material = BassRibbonMaterial;
+		this.gameObject.GetComponent<MeshRenderer> ().material.color = bassColor;
 		this.gameObject.GetComponent<MeshFilter> ().mesh = BassMesh.gameObject.GetComponent<MeshFilter> ().sharedMesh;
 		this.gameObject.transform.Rotate (-90f, 0f, 0f);
 	}
@@ -42,6 +104,7 @@ public class MarkerObjectBehavior : MonoBehaviour {
 	public void MelodyMarkerObject(){
 		
 		this.gameObject.GetComponent<MeshRenderer> ().material = MelodyRibbonMaterial;
+		this.gameObject.GetComponent<MeshRenderer> ().material.color = melodyColor;
 		this.gameObject.GetComponent<MeshFilter> ().mesh = MelodyMesh.gameObject.GetComponent<MeshFilter> ().sharedMesh;
 		this.gameObject.transform.Rotate (-90f, 0f, 0f);
 	
@@ -50,6 +113,7 @@ public class MarkerObjectBehavior : MonoBehaviour {
 	public void HarmonyMarkerObject(){
 		
 		this.gameObject.GetComponent<MeshRenderer> ().material = HarmonyRibbonMaterial;
+		this.gameObject.GetComponent<MeshRenderer> ().material.color = harmonyColor;
 		this.gameObject.GetComponent<MeshFilter> ().mesh = HarmonyMesh.gameObject.GetComponent<MeshFilter> ().sharedMesh;
 		this.gameObject.transform.Rotate (-90f, 0f, 0f);
 	
