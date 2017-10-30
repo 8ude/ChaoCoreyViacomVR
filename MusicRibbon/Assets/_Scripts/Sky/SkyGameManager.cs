32242e206b70dcd3674b17f11057d0257291076d
Democracy <ll3305@nyu.edu>
2017-10-30 19:09:15 -0400

    Basic function works right now!

--- MusicRibbon/Assets/_Scripts/Sky/SkyGameManager.cs
+++ MusicRibbon/Assets/_Scripts/Sky/SkyGameManager.cs
@@ -5,15 +5,19 @@ using VRTK;
 
 public class SkyGameManager : MonoBehaviour {
 
-	public GameObject SkyLineRender;
-	public int i;
+	public GameObject SkyLine;
+    public float Distance;
+    private LineRenderer skylinerender;
+    
+
+    public int i;
 
 	// Use this for initialization
 	void Start () {
 
+       skylinerender = SkyLine.GetComponent<LineRenderer>();
 
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,9 +26,14 @@ public class SkyGameManager : MonoBehaviour {
 
 	public void AddPoints(){
 
-		SkyLineRender.GetComponent<LineRenderer> ().SetPosition (i, this.gameObject.transform.position);
-		i = i + 1;
-		Debug.Log ("i:" + i + " position:" + this.gameObject.transform.position);
+        i = i + 1;
+        skylinerender.positionCount = i;
+
+        Vector3 newPosition = this.gameObject.transform.position + (Distance * transform.forward);
+        skylinerender.SetPosition (i-1, newPosition);
+		
+        
+		Debug.Log ("i:" + i + " position:" + newPosition);
 	}
 
 }
